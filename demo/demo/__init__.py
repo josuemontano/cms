7e2e3d535ab603aabbed86a23f37ade2bc9665a3
Josue Montano <josuemontano@intranet.local>
2013-10-15 10:44:33 -0400

    Shared models are referenced from spartan egg

--- demo/demo/__init__.py
+++ demo/demo/__init__.py
@@ -1,7 +1,7 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
-from demo.models.meta import (DBSession, Base)
+from .models.meta import (DBSession, Base)
 from .route import (page_exists, factory)
 from .views import (not_found, index)
 
--- demo/demo/route.py
+++ demo/demo/route.py
@@ -1,4 +1,4 @@
-from demo.models.meta import DBSession
+from .models.meta import DBSession
 from spartan.models.core import Page
 
 def get_url(info):
