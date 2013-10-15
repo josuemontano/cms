aa7319db112341a76a211f8ca861d9d04ab5e8a3
Josue Montano <josuemontano@intranet.local>
2013-10-15 10:45:34 -0400

    Created models.py

--- demo/demo/models/__init__.py
+++
@@ -1,2 +0,0 @@
-# package
-from .meta import *
--- demo/demo/models/meta.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-from sqlalchemy.orm import (scoped_session, sessionmaker)
-from sqlalchemy.ext.declarative import declarative_base
-from zope.sqlalchemy import ZopeTransactionExtension
-
-Base      = declarative_base()
-DBSession = scoped_session(sessionmaker())
