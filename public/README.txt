e82dd6663974b8c2e396d24d5dc7408c859f0cc3
Josue Montano <josuemontano@intranet.local>
2013-10-16 12:29:04 -0400

    Renamed package demo to public

---
+++ public/README.txt
@@ -0,0 +1,14 @@
+public README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $VENV/bin/python setup.py develop
+
+- $VENV/bin/initialize_public_db development.ini
+
+- $VENV/bin/pserve development.ini
+
---
+++ public/public/models.py
@@ -0,0 +1,6 @@
+from sqlalchemy.orm import (scoped_session, sessionmaker)
+from sqlalchemy.ext.declarative import declarative_base
+from zope.sqlalchemy import ZopeTransactionExtension
+
+Base      = declarative_base()
+DBSession = scoped_session(sessionmaker())
