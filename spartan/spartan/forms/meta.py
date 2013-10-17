150b72342326f89746c74295b8cb34ef1a7be0ff
Josue Montano <josuemontano@intranet.local>
2013-10-17 12:59:36 -0400

    Added files for custom models

--- spartan/spartan/forms/meta.py
+++ spartan/spartan/forms/meta.py
@@ -1,6 +1,7 @@
 from wtforms import *
 from wtforms.validators import *
 
+
 BLANK_REQUIRED    = 'Este campo no puede ser nulo'
 BLANK_USERNAME    = 'Debe ingresar un nombre de usuario'
 BLANK_PASSWORD    = 'La contraseña no puede ser nula'
@@ -8,11 +9,7 @@ SHORT_USERNAME    = 'El nombre de usuario debe constar de al menos 4 caracteres'
 SHORT_PASSWORD    = 'La contraseña debe constar de al menos 8 caracteres'
 PASSWORD_MISMATCH = 'Las contraseñas beben coincidir'
 
-
 class RequiredIf(Required):
-    # a validator which makes a field required if
-    # another field is set and has a truthy value
-
     def __init__(self, other_field_name, *args, **kwargs):
         self.other_field_name = other_field_name
         super(RequiredIf, self).__init__(*args, **kwargs)
