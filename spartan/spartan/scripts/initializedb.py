62f595e48b66d36676f9703a13ad546d40bff659
Josue Montano <josuemontano@Josues-MacBook-Pro.local>
2014-09-13 22:32:35 -0400

    CSS and JS optimized

--- spartan/spartan/scripts/initializedb.py
+++ spartan/spartan/scripts/initializedb.py
@@ -1,4 +1,3 @@
-import os
 import sys
 import transaction
 
@@ -10,6 +9,7 @@ from spartan.models.security import *
 from spartan.models.custom import *
 
 LOREM_IPSUM = '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam cursus. Morbi ut mi. Nullam enim leo, egestas id, condimentum at, laoreet mattis, massa. Sed eleifend nonummy diam. Praesent mauris ante, elementum et, bibendum at, posuere sit amet, nibh. Duis tincidunt lectus quis dui viverra vestibulum. Suspendisse vulputate aliquam dui. Nulla elementum dui ut augue. Aliquam vehicula mi at mauris. Maecenas placerat, nisl at consequat rhoncus, sem nunc gravida justo, quis eleifend arcu velit quis lacus. Morbi magna magna, tincidunt a, mattis non, imperdiet vitae, tellus. Sed odio est, auctor ac, sollicitudin in, consequat vitae, orci. Fusce id felis. Vivamus sollicitudin metus eget eros.</p><p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In posuere felis nec tortor. Pellentesque faucibus. Ut accumsan ultricies elit. Maecenas at justo id velit placerat molestie. Donec dictum lectus non odio. Cras a ante vitae enim iaculis aliquam. Mauris nunc quam, venenatis nec, euismod sit amet, egestas placerat, est. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras id elit. Integer quis urna. Ut ante enim, dapibus malesuada, fringilla eu, condimentum quis, tellus. Aenean porttitor eros vel dolor. Donec convallis pede venenatis nibh. Duis quam. Nam eget lacus. Aliquam erat volutpat. Quisque dignissim congue leo.</p><p>Mauris vel lacus vitae felis vestibulum volutpat. Etiam est nunc, venenatis in, tristique eu, imperdiet ac, nisl. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In iaculis facilisis massa. Etiam eu urna. Sed porta. Suspendisse quam leo, molestie sed, luctus quis, feugiat in, pede. Fusce tellus. Sed metus augue, convallis et, vehicula ut, pulvinar eu, ante. Integer orci tellus, tristique vitae, consequat nec, porta vel, lectus. Nulla sit amet diam. Duis non nunc. Nulla rhoncus dictum metus. Curabitur tristique mi condimentum orci. Phasellus pellentesque aliquam enim. Proin dui lectus, cursus eu, mattis laoreet, viverra sit amet, quam. Curabitur vel dolor ultrices ipsum dictum tristique. Praesent vitae lacus. Ut velit enim, vestibulum non, fermentum nec, hendrerit quis, leo. Pellentesque rutrum malesuada neque.</p><p>Maecenas aliquet velit vel turpis. Mauris neque metus, malesuada nec, ultricies sit amet, porttitor mattis, enim. In massa libero, interdum nec, interdum vel, blandit sed, nulla. In ullamcorper, est eget tempor cursus, neque mi consectetuer mi, a ultricies massa est sed nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Proin nulla arcu, nonummy luctus, dictum eget, fermentum et, lorem. Nunc porta convallis pede.</p>'
+
 
 def usage(argv):
     cmd = os.path.basename(argv[0])
@@ -30,7 +30,7 @@ def main(argv = sys.argv):
     Base.metadata.create_all(engine)
 
     with transaction.manager:
-        site = Site('Example', 'josuemontanoa@gmail.com; josue@talentoscreativos.com', ga_code = 'UA-XXXXXX-XX')
+        site = Site('Example', 'josuemontanoa@gmail.com', ga_code = 'UA-XXXXXX-XX')
         DBSession.add(site)
 
         group_a = Group('administrators')
@@ -38,7 +38,7 @@ def main(argv = sys.argv):
         DBSession.add(group_a)
         DBSession.add(group_b)
 
-        admin = User('josuemontanoa@gmail.com', 'admin1234', group_a)
+        admin = User('josuemontanoa@gmail.com', 'admin12345', group_a)
         DBSession.add(admin)
 
         template_a = PageTemplate('Inicio', 'home.html')
@@ -50,19 +50,19 @@ def main(argv = sys.argv):
         DBSession.add(template_c)
         DBSession.add(template_d)
 
-        home_page = Page(name = 'Inicio', description = 'This is my home page', content = LOREM_IPSUM)
+        home_page = Page(name='Inicio', description='This is my home page', content=LOREM_IPSUM)
         home_page.template = template_a
         DBSession.add(home_page)
 
-        page_a = Page(name = 'Quienes somos', description = 'This is my about us page', content = LOREM_IPSUM)
+        page_a = Page(name='Quienes somos', description='This is my about us page', content=LOREM_IPSUM)
         DBSession.add(page_a)
 
-        page_a1 = Page(name = 'Nuestras actividades', description = 'This is my about us page', content = LOREM_IPSUM, parent = page_a)
+        page_a1 = Page(name='Nuestras actividades', description='This is my about us page', content=LOREM_IPSUM, parent=page_a)
         DBSession.add(page_a1)
 
-        page_b = Page(name = 'Noticias', description = 'This is my news page', content = LOREM_IPSUM)
+        page_b = Page(name='Noticias', description='This is my news page', content=LOREM_IPSUM)
         DBSession.add(page_b)
 
-        page_c = Page(name = 'Contactos', description = 'This is my contact page')
+        page_c = Page(name='Contactos', description='This is my contact page')
         page_c.template = template_d
         DBSession.add(page_c)
